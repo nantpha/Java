bf954654396c9dc6a08d074fd428ef3c5b2034c5
nantpha <54667262+nantpha@users.noreply.github.com>
2024-04-04 08:03:16 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,92 +1,105 @@
-@
+General Purpose Instances (e.g., t3, m5):
 
-Sure, here's a suggested message:
+Use Cases: General-purpose instances are well-suited for a wide range of applications with balanced compute, memory, and networking performance requirements. They are ideal for web servers, small to medium databases, development environments, and enterprise applications.
+Example Instances:
+t3.nano, t3.micro, t3.small: Entry-level instances for low-traffic websites, small applications, and lightweight development environments.
+m5.large, m5.xlarge, m5.2xlarge: Suitable for applications requiring a balance of compute and memory resources like web servers and small to medium databases.
+Compute-Optimized Instances (e.g., c5, c6g):
 
-"Hey [Recipient's Name], just wanted to touch base regarding our recent deployments on Dec 1st and Dec 2nd. Are there any other production clusters that need deployment? If so, please let me know. Thanks!"
+Use Cases: Compute-optimized instances are designed for workloads requiring high-performance processing capabilities. They are suitable for CPU-intensive applications, batch processing, data analytics, and high-performance computing (HPC) workloads.
+Example Instances:
+c5.large, c5.xlarge, c5.2xlarge: Optimized for compute-intensive workloads such as data analytics and scientific simulations.
+c6g.large, c6g.xlarge, c6g.2xlarge: Powered by ARM-based Graviton2 processors, offering high-performance compute capabilities for various workloads like web servers and containerized applications.
+Memory-Optimized Instances (e.g., r5, x1e):
+
+Use Cases: Memory-optimized instances are ideal for applications demanding large amounts of RAM. They are suitable for in-memory databases, real-time analytics, and memory-intensive applications.
+Example Instances:
+r5.large, r5.xlarge, r5.2xlarge: Optimized for applications requiring a balance of compute and memory resources, such as in-memory databases and real-time analytics.
+x1e.xlarge, x1e.2xlarge, x1e.4xlarge: Instances with large memory sizes, suitable for memory-intensive workloads like SAP HANA and in-memory databases.
+Storage-Optimized Instances (e.g., i3, d2):
+
+Use Cases: Storage-optimized instances are designed for applications demanding high-performance storage subsystems. They are suitable for big data processing, data warehousing, and distributed file systems.
+Example Instances:
+i3.large, i3.xlarge, i3.2xlarge: Optimized for I/O-intensive applications requiring high-performance local storage, such as NoSQL databases and data warehousing.
+d2.large, d2.xlarge, d2.2xlarge: Ideal for applications requiring high-density, sequential read and write access to large datasets, such as Hadoop clusters and distributed file systems.
+ 
+
+•	Kinesis Data Streams: Allows you to collect and process large streams of data records in real-time. It can scale elastically to handle varying levels of throughput.
+•	Kinesis Data Firehose: Enables you to reliably load streaming data into data lakes, data stores, and analytics services without managing infrastructure. It can automatically scale to accommodate data ingestion.
+•	Kinesis Data Analytics: Provides the capability to analyze streaming data using SQL or Apache Flink. It allows you to process and analyze data in real-time, making it suitable for performing analytics on streaming data.
 
 
+Amazon S3 (Simple Storage Service):
 
-if "%DEBUG%" == "" @echo off
-@rem ##########################################################################
-@rem
-@rem  Gradle startup script for Windows
-@rem
-@rem ##########################################################################
+Purpose: Amazon S3 is an object storage service designed to store and retrieve any amount of data from anywhere on the web. It provides highly durable and scalable storage for a variety of data types, including images, videos, documents, backups, and application data.
+Use Cases:
+Storing static content for websites: S3 can be used to store images, CSS files, JavaScript files, and other static assets used by web applications.
+Data backup and archiving: S3 is commonly used for backing up and archiving data due to its durability, scalability, and cost-effectiveness.
+Content distribution: S3 integrates with Amazon CloudFront to deliver content globally with low latency and high transfer speeds.
+Big data analytics: S3 serves as a data lake for storing large volumes of data for analytics purposes, including data lakes, data warehousing, and machine learning.
+Amazon EBS (Elastic Block Store):
 
-@rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal
+Purpose: Amazon EBS provides block-level storage volumes that can be attached to EC2 instances. It offers persistent, high-performance storage for EC2 instances, similar to a traditional hard drive or SSD.
+Use Cases:
+Boot volumes for EC2 instances: EBS volumes are commonly used as boot volumes for EC2 instances, providing the operating system and application data.
+Database storage: EBS volumes are suitable for storing database files for relational databases (e.g., MySQL, PostgreSQL) and NoSQL databases (e.g., MongoDB, Cassandra).
+Transactional workloads: EBS volumes are optimized for transactional workloads that require low-latency, high-IOPS storage, such as OLTP (Online Transaction Processing) databases and applications.
+Block-level storage: EBS volumes support block-level operations, allowing them to be formatted with a file system and used like a physical disk drive attached to a server.
 
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=
+Amazon S3 (Simple Storage Service) ensures durability and availability of objects stored within it through various mechanisms and redundancy options. Here's how Amazon S3 achieves durability and availability:
 
-@rem Find java.exe
-if defined JAVA_HOME goto findJavaFromJavaHome
+Data Replication and Redundancy:
 
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%" == "0" goto init
+Amazon S3 automatically replicates data across multiple facilities within a region to ensure redundancy and fault tolerance. This replication process is managed by Amazon S3, and customers do not need to configure it.
+Objects stored in Amazon S3 are redundantly stored across multiple devices and facilities to protect against hardware failures, errors, and outages.
+Checksums and Error Checking:
 
-echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+Amazon S3 uses checksums to detect and correct errors during data transfer and storage. Each object stored in Amazon S3 has a unique checksum (ETag), which is calculated based on the object's contents.
+When an object is uploaded or retrieved from Amazon S3, the service verifies the integrity of the data using the checksum. If any errors are detected, Amazon S3 automatically corrects them using redundant copies of the data.
+Data Availability:
 
-goto fail
+Amazon S3 is designed for high availability, offering a service level agreement (SLA) for 99.99% uptime. This ensures that objects stored in Amazon S3 are highly available and accessible whenever needed.
+Amazon S3 provides low-latency access to data, allowing applications to retrieve objects quickly and reliably.
+Storage Classes:
 
-:findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+Amazon S3 offers a range of storage classes, each optimized for different access patterns, durability, availability, and cost requirements. The storage classes offered by Amazon S3 include:
+Standard: This is the default storage class for Amazon S3. It offers high durability, availability, and performance for frequently accessed data.
+Standard-IA (Infrequent Access): This storage class is suitable for data that is accessed less frequently but requires rapid access when needed. It offers lower storage costs compared to the standard storage class.
+One Zone-IA: Similar to Standard-IA but stores data in a single availability zone, offering lower storage costs with slightly reduced availability compared to Standard-IA.
+Intelligent-Tiering: This storage class automatically moves objects between two access tiers (frequent access and infrequent access) based on their access patterns. It optimizes storage costs while maintaining performance.
+Glacier: Glacier is designed for long-term archival and backup storage. It offers extremely low storage costs but longer retrieval times compared to other storage classes.
+Glacier Deep Archive: This storage class is optimized for data that is rarely accessed and requires the lowest storage costs. It offers the lowest retrieval costs but with the longest retrieval times.
+By offering a range of storage classes, Amazon S3 allows customers to choose the appropriate class based on their specific requirements for durability, availability, performance, and cost. This flexibility enables customers to optimize their storage costs while ensuring their data remains highly durable and available.
 
-if exist "%JAVA_EXE%" goto init
+Amazon VPC (Virtual Private Cloud) is a service provided by Amazon Web Services (AWS) that allows users to create a virtual network in the cloud, isolated from other virtual networks. It enables users to launch AWS resources, such as EC2 instances, RDS databases, and Lambda functions, within a defined virtual network.
 
-echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+Here's how Amazon VPC works and the components that make up a VPC:
 
-goto fail
+Subnets:
 
-:init
-@rem Get command-line arguments, handling Windows variants
+Subnets are subdivisions of an Amazon VPC. They represent segmented portions of the IP address range of the VPC and are associated with a specific availability zone within a region.
+Subnets allow users to group resources based on their functionality or security requirements. For example, you might have a public subnet for resources that need to be accessible from the internet and a private subnet for resources that should not be directly accessible from the internet.
+Internet Gateway (IGW):
 
-if not "%OS%" == "Windows_NT" goto win9xME_args
+An Internet Gateway is a horizontally scaled, redundant, and highly available VPC component that allows communication between instances in the VPC and the internet.
+Internet Gateways enable instances in public subnets to connect to the internet for tasks such as downloading updates, accessing SaaS applications, or serving web traffic.
+Route Tables:
 
-:win9xME_args
-@rem Slurp the command line arguments.
-set CMD_LINE_ARGS=
-set _SKIP=2
+Route Tables define the routing configuration for subnets within a VPC. Each subnet is associated with a route table, which specifies how traffic should be directed within the VPC and to external destinations.
+Users can create custom route tables to define specific routing rules. For example, a public subnet might have a route to the Internet Gateway, while a private subnet might have a route to a NAT Gateway or NAT instance for outbound internet access.
+Network Access Control Lists (NACLs):
 
-:win9xME_args_slurp
-if "x%~1" == "x" goto execute
+NACLs are stateless firewall rules that control traffic entering and leaving subnets in a VPC. They provide an additional layer of security by allowing or denying traffic based on IP addresses, ports, and protocols.
+NACLs are applied at the subnet level and operate independently of security groups. They are evaluated in a sequential order, with rules applied based on the first matching rule.
+Security Groups:
 
-set CMD_LINE_ARGS=%*
+Security Groups act as virtual firewalls for EC2 instances and other AWS resources within a VPC. They control inbound and outbound traffic at the instance level by specifying allowed traffic based on rules.
+Security Groups are stateful, meaning they automatically allow return traffic related to allowed inbound traffic. They are associated with instances and provide granular control over traffic based on protocols, ports, and IP addresses.
+Virtual Private Gateway (VGW):
 
-:execute
-@rem Setup the command line
+A Virtual Private Gateway is a VPN concentrator on the Amazon side of a VPN connection. It allows users to establish secure connections between their on-premises networks and their Amazon VPCs.
+Virtual Private Gateways enable users to extend their on-premises networks into the AWS cloud securely, facilitating hybrid cloud architectures and migration scenarios.
+By leveraging these components, users can create isolated and secure network environments in the AWS cloud, customize routing and access controls, and connect their VPCs to external networks or on-premises infrastructure as needed. This flexibility enables users to design and deploy complex architectures while maintaining control over network security and connectivity.
 
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-@rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
-
-:end
-@rem End local scope for the variables with windows NT shell
-if "%ERRORLEVEL%"=="0" goto mainEnd
-
-:fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c_ return code!
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
-exit /b 1
-
-:mainEnd
-if "%OS%"=="Windows_NT" endlocal
-
-:omega
